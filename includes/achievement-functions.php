0d7421c9ce2912a0605c20f57079241c034da3e1
Brian Richards <brian@rzen.net>
2013-05-03 10:38:41 -0400

    Only generate earners output if we have earners

--- includes/achievement-functions.php
+++ includes/achievement-functions.php
@@ -596,16 +596,20 @@ function badgeos_get_achievement_earners( $achievement_id = 0 ) {
 function badgeos_get_achievement_earners_list( $achievement_id = 0 ) {
 
 	// Grab our users
-	$users = badgeos_get_achievement_earners( $achievement_id );
+	$earners = badgeos_get_achievement_earners( $achievement_id );
+	$output = '';
 
-	// Loop through each user and build our output
-	$output = '<h4>' . apply_filters( 'badgeos_earners_heading', __( 'People Who Have Earned This', 'badgeos' ) ) . '</h4>';
-	$output .= '<ul class="badgeos-achievement-earners-list achievement-' . $achievement_id . '-earners-list">';
-	foreach ( $users as $user ) {
-		$user_content = '<li><a href="' . get_author_posts_url( $user->ID ) . '">' . get_avatar( $user->ID ) . '</a></li>';
-		$output .= apply_filters( 'badgeos_get_achievement_earners_list_user', $user_content, $user->ID );
+	// Only generate output if we have earners
+	if ( ! empty( $earners ) )  {
+		// Loop through each user and build our output
+		$output .= '<h4>' . apply_filters( 'badgeos_earners_heading', __( 'People Who Have Earned This', 'badgeos' ) ) . '</h4>';
+		$output .= '<ul class="badgeos-achievement-earners-list achievement-' . $achievement_id . '-earners-list">';
+		foreach ( $earners as $user ) {
+			$user_content = '<li><a href="' . get_author_posts_url( $user->ID ) . '">' . get_avatar( $user->ID ) . '</a></li>';
+			$output .= apply_filters( 'badgeos_get_achievement_earners_list_user', $user_content, $user->ID );
+		}
+		$output .= '</ul>';
 	}
-	$output .= '</ul>';
 
 	// Return our concatenated output
 	return apply_filters( 'badgeos_get_achievement_earners_list', $output, $achievement_id, $users );
