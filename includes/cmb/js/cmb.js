826e0a822b382fce2f706e3d267f8cc182bb5008
Brian Richards <brian@rzen.net>
2013-09-14 20:06:39 -0400

    disable CMB JS that modifies the 'insert media' behavior because it conflicts with Canvas by WooThemes, and becuase we don't need it in core

--- includes/cmb/js/cmb.js
+++ includes/cmb/js/cmb.js
@@ -41,7 +41,7 @@ jQuery(document).ready(function ($) {
 	});
 	// Wrap date picker in class to narrow the scope of jQuery UI CSS and prevent conflicts
 	$("#ui-datepicker-div").wrap('<div class="cmb_element" />');
-	
+
 	/**
 	 * Initialize color picker
 	 */
@@ -80,49 +80,57 @@ jQuery(document).ready(function ($) {
 		return false;
 	});
 
-	window.original_send_to_editor = window.send_to_editor;
-    window.send_to_editor = function (html) {
-		var itemurl, itemclass, itemClassBits, itemid, htmlBits, itemtitle,
-			image, uploadStatus = true;
+	/**
+	 * The code below modifies the "insert media" behavior.
+	 *
+	 * It has been commented out because WooTheme's Canvas theme
+	 * causes conflict because they duplicate this code. We currently
+	 * don't use it for BadgeOS in any way, so there is no harm
+	 * in removing it for now. -BR
+	 */
+	// window.original_send_to_editor = window.send_to_editor;
+	// window.send_to_editor = function (html) {
+	// 	var itemurl, itemclass, itemClassBits, itemid, htmlBits, itemtitle,
+	// 		image, uploadStatus = true;
 
-		if (formfield) {
+	// 	if (formfield) {
 
-	        if ($(html).html(html).find('img').length > 0) {
-				itemurl = $(html).html(html).find('img').attr('src'); // Use the URL to the size selected.
-				itemclass = $(html).html(html).find('img').attr('class'); // Extract the ID from the returned class name.
-				itemClassBits = itemclass.split(" ");
-				itemid = itemClassBits[itemClassBits.length - 1];
-				itemid = itemid.replace('wp-image-', '');
-	        } else {
-				// It's not an image. Get the URL to the file instead.
-				htmlBits = html.split("'"); // jQuery seems to strip out XHTML when assigning the string to an object. Use alternate method.
-				itemurl = htmlBits[1]; // Use the URL to the file.
-				itemtitle = htmlBits[2];
-				itemtitle = itemtitle.replace('>', '');
-				itemtitle = itemtitle.replace('</a>', '');
-				itemid = ""; // TO DO: Get ID for non-image attachments.
-			}
+	// 		if ($(html).html(html).find('img').length > 0) {
+	// 			itemurl = $(html).html(html).find('img').attr('src'); // Use the URL to the size selected.
+	// 			itemclass = $(html).html(html).find('img').attr('class'); // Extract the ID from the returned class name.
+	// 			itemClassBits = itemclass.split(" ");
+	// 			itemid = itemClassBits[itemClassBits.length - 1];
+	// 			itemid = itemid.replace('wp-image-', '');
+	// 		} else {
+	// 			// It's not an image. Get the URL to the file instead.
+	// 			htmlBits = html.split("'"); // jQuery seems to strip out XHTML when assigning the string to an object. Use alternate method.
+	// 			itemurl = htmlBits[1]; // Use the URL to the file.
+	// 			itemtitle = htmlBits[2];
+	// 			itemtitle = itemtitle.replace('>', '');
+	// 			itemtitle = itemtitle.replace('</a>', '');
+	// 			itemid = ""; // TO DO: Get ID for non-image attachments.
+	// 		}
 
-			image = /(jpe?g|png|gif|ico)$/gi;
+	// 		image = /(jpe?g|png|gif|ico)$/gi;
 
-			if (itemurl.match(image)) {
-				uploadStatus = '<div class="img_status"><img src="' + itemurl + '" alt="" /><a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove Image</a></div>';
-			} else {
-				// No output preview if it's not an image
-				// Standard generic output if it's not an image.
-				html = '<a href="' + itemurl + '" target="_blank" rel="external">View File</a>';
-				uploadStatus = '<div class="no_image"><span class="file_link">' + html + '</span>&nbsp;&nbsp;&nbsp;<a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove</a></div>';
-			}
+	// 		if (itemurl.match(image)) {
+	// 			uploadStatus = '<div class="img_status"><img src="' + itemurl + '" alt="" /><a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove Image</a></div>';
+	// 		} else {
+	// 			// No output preview if it's not an image
+	// 			// Standard generic output if it's not an image.
+	// 			html = '<a href="' + itemurl + '" target="_blank" rel="external">View File</a>';
+	// 			uploadStatus = '<div class="no_image"><span class="file_link">' + html + '</span>&nbsp;&nbsp;&nbsp;<a href="#" class="cmb_remove_file_button" rel="' + formfield + '">Remove</a></div>';
+	// 		}
 
-			$('#' + formfield).val(itemurl);
-			$('#' + formfield + '_id').val(itemid);
-			$('#' + formfield).siblings('.cmb_upload_status').slideDown().html(uploadStatus);
-			tb_remove();
+	// 		$('#' + formfield).val(itemurl);
+	// 		$('#' + formfield + '_id').val(itemid);
+	// 		$('#' + formfield).siblings('.cmb_upload_status').slideDown().html(uploadStatus);
+	// 		tb_remove();
 
-		} else {
-			window.original_send_to_editor(html);
-		}
+	// 	} else {
+	// 		window.original_send_to_editor(html);
+	// 	}
 
-		formfield = '';
-	};
+	// 	formfield = '';
+	// };
 });
