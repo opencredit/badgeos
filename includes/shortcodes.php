52506a672ffbf2fd0db04ffeeec288dfa1529ecc
Brian Richards <brian@rzen.net>
2013-11-29 11:33:33 -0500

    Add sanitization to assertion shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -498,7 +498,7 @@ function badgeos_credly_assertion_page( $atts = array() ) {
 
 	// Setup defaults
 	$defaults = array(
-		'CID'     => isset( $_GET['CID'] ) ? absint( $_GET['CID'] ) : 0,
+		'CID'    => isset( $_GET['CID'] ) ? absint( $_GET['CID'] ) : 0,
 		'width'  => isset( $content_width ) ? $content_width : 560,
 		'height' => 1000,
 	);
@@ -507,8 +507,8 @@ function badgeos_credly_assertion_page( $atts = array() ) {
 	$atts = shortcode_atts( $defaults, $atts );
 
 	// If passed an ID, render the iframe, otherwise render nothing
-	if ( $atts['CID'] )
-		return '<iframe class="credly-assertion" src="http://credly.com/credit/' . $atts['CID'] . '/embed" align="top" marginwidth="0" width="' . $atts['width'] . 'px" height="' . $atts['height'] . 'px" scrolling="no" frameborder="no"></iframe>';
+	if ( absint( $atts['CID'] ) )
+		return '<iframe class="credly-assertion" src="http://credly.com/credit/' . absint( $atts['CID'] ) . '/embed" align="top" marginwidth="0" width="' . absint( $atts['width'] ) . 'px" height="' . absint( $atts['height'] ) . 'px" scrolling="no" frameborder="no"></iframe>';
 	else
 		return '';
 
