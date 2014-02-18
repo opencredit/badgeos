291f3ffc677255fa8f8bf359e6650c64cae54c1d
Michael Beckwith <tw2113@gmail.com>
2014-02-18 14:08:13 -0600

    add our new base shortcode class

--- badgeos.php
+++ badgeos.php
@@ -67,6 +67,7 @@ class BadgeOS {
 		require_once( $this->directory_path . 'includes/p2p/load.php' );
 		require_once( $this->directory_path . 'includes/class.BadgeOS_Editor_Shortcodes.php' );
 		require_once( $this->directory_path . 'includes/class.BadgeOS_Plugin_Updater.php' );
+		require_once( $this->directory_path . 'includes/class.BadgeOS_Shortcode.php' );
 		require_once( $this->directory_path . 'includes/class.Credly_Badge_Builder.php' );
 		require_once( $this->directory_path . 'includes/post-types.php' );
 		require_once( $this->directory_path . 'includes/admin-settings.php' );
