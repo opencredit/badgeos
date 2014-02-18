fec662356ea76cefe7b06d5a686dbf4049b19837
Michael Beckwith <tw2113@gmail.com>
2014-02-17 23:59:01 -0600

    better comments with more explanation

--- js/badgeos-shortcode-embed.js
+++ js/badgeos-shortcode-embed.js
@@ -1,14 +1,14 @@
 (function($) {
 
 	/*
-	Construct and return our attributes list for the provided shortcode.
+	Construct and return our attributes list for the provided shortcode. Grabs all results from a localized variable.
 	 */
 	function badgeos_get_attributes( shortcode ) {
-		//badgeos_shortcodes will be a localized variable
 		var result = {};
 
 		result.shortcode = shortcode;
 
+		//If the localized variable has the requested property aka requested shortcode, add those to our params property.
 		if ( badgeos_shortcodes.hasOwnProperty(shortcode) ) {
 			result.params = badgeos_shortcodes[shortcode];
 
@@ -17,15 +17,18 @@
 	}
 
 	/*
-	Set the values from the inputs for the provided shortcode.
+	Set the values from the inputs for the provided shortcode. This constructs all of the attributes that have user-set values.
 	 */
 	function badgeos_set_current_attributes( shortcode ){
+		//Keep that global namespace clean.
 		var attributes = badgeos_get_attributes( shortcode ), result = {}, params = [], input = '', value = '';
 
 		result.shortcode = attributes.shortcode;
 
+		//If we have any available parameters
 		if ( attributes.params ) {
 			for( i = 0; i < attributes.params.length; i++ ) {
+				//Had to grab the values out of the select input differently.
 				if ( 'bool' === attributes.params[i].type ) {
 					input = $('#badgeos_'+attributes.params[i].param+' option:selected' ).val();
 				} else {
@@ -33,29 +36,33 @@
 				}
 				value = ( input.length > 0 ) ? input : null;
 
+				//If we had a value at all, push it intou our params array.
 				if ( value ) {
 					params.push( attributes.params[i].param+'="'+value+'"' );
 				}
 			}
+			//assign the params property with our array of found parameters.
 			result.params = params;
 		}
 		return result;
 	}
 
 	/*
-	Construct our final shortcode string and return it to the WP Editor.
+	Construct our final shortcode string and return it to the WP Editor. This gets called when the user clicks "Insert Shortcode".
 	 */
 	function badgeos_insert_shortcode(){
 		var shortcode, attributes;
 
+		//Grab our requested shortcode from the dropdown, set the attributes for the chosen.
 		shortcode = $( '#select_shortcode option:selected' ).val();
 		requested = badgeos_set_current_attributes( shortcode );
 
+		//Insert our final result into the post editor, after construction.
 		window.send_to_editor( badgeos_construct_shortcode( requested ) );
 	}
 
 	/*
-	Concatenate all of our attributes into one string.
+	Concatenate all of our attributes into one string. This will set only user-set attributes
 	 */
 	function badgeos_construct_shortcode( requested ) {
 		var shortcode = '[';
@@ -73,7 +80,7 @@
 		return shortcode;
 	}
 	/*
-	Construct the html inputs and append to our output
+	Create all of the inputs for the requested shortcode. Inputs will provide no default values.
 	 */
 	function badgeos_construct_input( shortcode ) {
 		inputs = '';
@@ -129,15 +136,16 @@
 		$( '#shortcode_options' ).html( inputs );
 	});
 
-	//Reset some values upon clicking.
+	//Set/reset some values upon clicking.
 	$('#insert_badgeos_shortcodes').on( 'click', function(){
 		$( '#select_shortcode' ).val( 'unselected' );
 		$( '#shortcode_options' ).html('');
 	});
-	//Handle click events in our modal popup.
+	//Insert constructed shortcode and close popup
 	$( '#badgeos_insert' ).on( 'click', function(){
 		badgeos_insert_shortcode();
 	});
+	//Close the modal popup without doing anything.
 	$( '#badgeos_cancel' ).on( 'click', function(e){
 		e.preventDefault();
 		tb_remove();
