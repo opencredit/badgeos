228ad01508316846b22c1d32a024a5722d2e133d
Michael Beckwith <tw2113@gmail.com>
2014-02-18 23:38:14 -0600

    as it turns out, the assertion page shortcode does have shortcodes

--- js/badgeos-shortcode-embed.js
+++ js/badgeos-shortcode-embed.js
@@ -153,10 +153,7 @@
 	$( '#select_shortcode' ).on( 'change', function(){
 		var selected = $( '#select_shortcode option:selected' ).val(), html = '';
 
-		if ( 'credly_assertion_page' == selected ) {
-			$( '#shortcode_options' ).html( '<p>'+badgeos_shortcode_messages.noparams+'</p>' );
-			return;
-		} else if ( 'unselected' === selected ) {
+		if ( 'unselected' === selected ) {
 			$( '#shortcode_options' ).html( '<p>'+badgeos_shortcode_messages.starting+'</p>' );
 			return;
 		}
