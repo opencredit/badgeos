9f4bdc6e64d6004b0a8e7d09ff889988b6705cc5
Michael Beckwith <tw2113@gmail.com>
2014-03-11 14:29:32 -0500

    wrap in an init hook, types changed, values set appropriately

--- includes/shortcodes/credly_assertion_page.php
+++ includes/shortcodes/credly_assertion_page.php
@@ -1,28 +1,31 @@
 <?php
 
-$credly_assertion_shortcode = badgeos_register_shortcode( array(
-	'name' => __( 'Credly Assertion Page', 'badgeos' ),
-	'slug' => 'credly_assertion_page',
-	'description' => __( 'Shortcode for rendering Credly Assertion page content.', 'badgeos' ),
-	'attributes' => array(
-		'CID' => array(
-			'name' => __( 'CID', 'badgeos' ),
-			'type' => 'string',
-			'description' => __( 'Credly assertion ID to display. Default: CID $_GET parameter or 0', 'badgeos' )
-			),
-		'width' => array(
-			'name' => __( 'Width', 'badgeos' ),
-			'type' => 'string',
-			'description' => __( 'Width to display the rendered shortcode at. Default: $content_width value if set or 560', 'badgeos' )
-			),
-		'height' => array(
-			'name' => __( 'Height', 'badgeos' ),
-			'type' => 'string',
-			'description' => __( 'Height to display the rendered shortcode. Default: 1000', 'badgeos' )
-			),
-	),
-	'output_callback' => 'badgeos_credly_assertion_page'
-) );
+function badgeos_register_credly_assertion_shortcode() {
+	$credly_assertion_shortcode = badgeos_register_shortcode( array(
+		'name' => __( 'Credly Assertion Page', 'badgeos' ),
+		'slug' => 'credly_assertion_page',
+		'description' => __( 'Shortcode for rendering Credly Assertion page content.', 'badgeos' ),
+		'attributes' => array(
+			'CID' => array(
+				'name' => __( 'CID', 'badgeos' ),
+				'type' => 'text',
+				'description' => __( 'Credly assertion ID to display. Default: CID $_GET parameter or 0', 'badgeos' )
+				),
+			'width' => array(
+				'name' => __( 'Width', 'badgeos' ),
+				'type' => 'text',
+				'description' => __( 'Width to display the rendered shortcode at. Default: $content_width value if set or 560', 'badgeos' )
+				),
+			'height' => array(
+				'name' => __( 'Height', 'badgeos' ),
+				'type' => 'text',
+				'description' => __( 'Height to display the rendered shortcode. Default: 1000', 'badgeos' )
+				),
+		),
+		'output_callback' => 'badgeos_credly_assertion_page'
+	) );
+}
+add_action( 'init', 'badgeos_register_credly_assertion_shortcode' );
 
 /**
  * Shortcode for rendering Credly Assertion page content.
