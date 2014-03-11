a9ba745924fcca21cf6bcaa37ef8af2478cc8a9c
Scott Kingsley Clark <lol@scottkclark.com>
2014-03-11 15:08:58 -0500

    Comment trigger work

--- includes/triggers.php
+++ includes/triggers.php
@@ -22,7 +22,8 @@ function badgeos_get_activity_triggers() {
 		array(
 			// WordPress-specific
 			'wp_login'             => __( 'Log in to Website', 'badgeos' ),
-			'comment_post'         => __( 'Comment on a post', 'badgeos' ),
+			'badgeos_new_comment'          => __( 'Comment on a post', 'badgeos' ),
+			'badgeos_new_comment_specific' => __( 'Comment on a specific post', 'badgeos' ),
 			'badgeos_new_post'     => __( 'Publish a new post', 'badgeos' ),
 			'badgeos_new_page'     => __( 'Publish a new page', 'badgeos' ),
 
@@ -143,8 +144,17 @@ function badgeos_trigger_get_user_id( $trigger = '', $args = array() ) {
 			$user_id = $args[0];
 			break;
 		case 'badgeos_new_post' :
+		case 'badgeos_specific_new_post' :
 		case 'badgeos_new_page' :
+		case 'badgeos_specific_new_page' :
 			$user_id = $args[1];
+			break;
+		case 'badgeos_new_comment' :
+		case 'badgeos_specific_new_comment' :
+			$user_id = 0;
+			if ( is_array( $args[ 1 ] ) && isset( $args[ 1 ][ 'user_id' ] ) ) {
+				$user_id = $args[ 1 ][ 'user_id' ];
+			}
 			break;
 		default :
 			$user_id = get_current_user_id();
@@ -303,7 +313,29 @@ function badgeos_publish_listener( $post_id = 0 ) {
 
 	// Trigger a badgeos_new_{$post_type} action
 	$post = get_post( $post_id );
-	do_action( "badgeos_new_{$post->post_type}", $post_id, $post->post_author );
+
+	do_action( "badgeos_specific_new_{$post->post_type}", $post_id, $post->post_author, $post );
+	do_action( "badgeos_new_{$post->post_type}", $post_id, $post->post_author, $post );
+
 }
 add_action( 'publish_post', 'badgeos_publish_listener', 0 );
 add_action( 'publish_page', 'badgeos_publish_listener', 0 );
+
+/**
+ * Listener function for comment publishing
+ *
+ * This triggers separate hooks: badgeos_new_comment, badgeos_new_comment_specific
+ *
+ * @since  1.4.0
+ * @param  integer $comment_ID The comment ID
+ * @param  array $comment The comment array
+ * @return void
+ */
+function badgeos_approved_comment_listener( $comment_ID, $comment ) {
+
+	// Trigger a comment actions
+	do_action( 'badgeos_specific_new_comment', $comment_ID, $comment[ 'user_id' ], $comment[ 'comment_post_ID' ], $comment );
+	do_action( 'badgeos_new_comment', $comment_ID, $comment[ 'user_id' ], $comment );
+
+}
+add_action( 'comment_approved_comment', 'badgeos_approved_comment_listener', 0, 2 );
