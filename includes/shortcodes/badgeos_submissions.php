d1a3345917788fcd285b7df4617765f170e9c7a7
Michael Beckwith <tw2113@gmail.com>
2014-03-11 14:29:21 -0500

    wrap in an init hook, types changed, values set appropriately

--- includes/shortcodes/badgeos_submissions.php
+++ includes/shortcodes/badgeos_submissions.php
@@ -1,52 +1,62 @@
 <?php
 
-$badgeos_submissions_list_shortcode = badgeos_register_shortcode( array(
-	'name' => __( 'BadgeOS Submissions List', 'badgeos' ),
-	'slug' => 'badgeos_submissions',
-	'description' => __( 'Display a filterable list of submissions', 'badgeos' ),
-	'attributes' => array(
-		'type' => array(
-			'name' => __( 'Type', 'badgeos' ),
-			'type' => 'string',
-			'description' => __( 'Achievement type to list submissions for. Default: submission', 'badgeos' )
-			),
-		'limit' => array(
-			'name' => __( 'Limit', 'badgeos' ),
-			'type' => 'integer',
-			'description' => __( 'How many submissions to list at a time. Default: 10', 'badgeos' )
-			),
-		'status' => array(
-			'name' => __( 'Status', 'badgeos' ),
-			'type' => 'string',
-			'description' => __( 'Achievement status to query for. Default: all', 'badgeos' )
-			),
-		'show_filter' => array(
-			'name' => __( 'Show Filter', 'badgeos' ),
-			'type' => 'boolean',
-			'description' => __( 'Whether or not to render filter controls. Default: true', 'badgeos' ),
-			'default' => true
-			),
-		'show_search' => array(
-			'name' => __( 'Show Search', 'badgeos' ),
-			'type' => 'boolean',
-			'description' => __( 'Whether or not to render search controls. Default: true', 'badgeos' ),
-			'default' => true
-			),
-		'show_attachments' => array(
-			'name' => __( 'Show attachments', 'badgeos' ),
-			'type' => 'boolean',
-			'description' => __( 'Whether or not to display submitted attachments. Default: true', 'badgeos' ),
-			'default' => true
-			),
-		'show_comments' => array(
-			'name' => __( 'Show comments', 'badgeos' ),
-			'type' => 'boolean',
-			'description' => __( 'Whether or not to display submitted comments. Default: true', 'badgeos' ),
-			'default' => true
-			),
-	),
-	'output_callback' => 'badgeos_display_submissions'
-) );
+function badgeos_register_submissions_list_shortcode() {
+	$badgeos_submissions_list_shortcode = badgeos_register_shortcode( array(
+		'name' => __( 'BadgeOS Submissions List', 'badgeos' ),
+		'slug' => 'badgeos_submissions',
+		'description' => __( 'Display a filterable list of submissions', 'badgeos' ),
+		'attributes' => array(
+			'type' => array(
+				'name' => __( 'Type', 'badgeos' ),
+				'type' => 'select',
+				'description' => __( 'Achievement type to list submissions for. Default: submission', 'badgeos' ),
+				'values' => badgeos_get_awardable_achievements()
+				),
+			'limit' => array(
+				'name' => __( 'Limit', 'badgeos' ),
+				'type' => 'text',
+				'description' => __( 'How many submissions to list at a time. Default: 10', 'badgeos' )
+				),
+			'status' => array(
+				'name' => __( 'Status', 'badgeos' ),
+				'type' => 'select',
+				'description' => __( 'Submission status to query for. Default: all', 'badgeos' ),
+				'values' => array( 'all', 'approved', 'denied', 'pending' ),
+				'default' => 'all'
+				),
+			'show_filter' => array(
+				'name' => __( 'Show Filter', 'badgeos' ),
+				'type' => 'select_bool',
+				'description' => __( 'Whether or not to render filter controls. Default: true', 'badgeos' ),
+				'values' => array( 'true', 'false' ),
+				'default' => 'true'
+				),
+			'show_search' => array(
+				'name' => __( 'Show Search', 'badgeos' ),
+				'type' => 'select_bool',
+				'description' => __( 'Whether or not to render search controls. Default: true', 'badgeos' ),
+				'values' => array( 'true', 'false' ),
+				'default' => 'true'
+				),
+			'show_attachments' => array(
+				'name' => __( 'Show attachments', 'badgeos' ),
+				'type' => 'select_bool',
+				'description' => __( 'Whether or not to display submitted attachments. Default: true', 'badgeos' ),
+				'values' => array( 'true', 'false' ),
+				'default' => 'true'
+				),
+			'show_comments' => array(
+				'name' => __( 'Show comments', 'badgeos' ),
+				'type' => 'select_bool',
+				'description' => __( 'Whether or not to display submitted comments. Default: true', 'badgeos' ),
+				'values' => array( 'true', 'false' ),
+				'default' => 'true'
+				),
+		),
+		'output_callback' => 'badgeos_display_submissions'
+	) );
+}
+add_action( 'init', 'badgeos_register_submissions_list_shortcode');
 
 /**
  * Shortcode to display a filterable list of Submissions
