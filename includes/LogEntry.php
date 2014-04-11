a118ace89187ff0670f9812ebd7e1e15e83c9d53
Kris Arnold <karnold@dma.org>
2014-04-11 10:23:03 -0500

    created user scope for log entries

--- includes/LogEntry.php
+++ includes/LogEntry.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class LogEntry extends Model {
     protected $table = 'badgeos_logs';
     public $timestamps = false;
+
+    public function scopeUser($query, $user_id) {
+        return $query->where('user_id', $user_id);
+    }
 }
