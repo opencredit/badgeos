7fd0e0c11e474b6d4cd50db045cebc2b85913b12
Brian Richards <brian@rzen.net>
2014-04-25 10:07:27 -0400

    Add baseline for all new tests

---
+++ tests/phpunit/tests/BadgeOS_Credly_Helpers_Test.php
@@ -0,0 +1,40 @@
+<?php
+
+class BadgeOS_Credly_Helpers_Test extends WP_UnitTestCase {
+
+	/**
+	 * @covers credly_issue_badge()
+	 */
+	public function credly_issue_badge() {}
+
+	/**
+	 * @covers badgeos_credly_get_api_key()
+	 */
+	public function badgeos_credly_get_api_key() {}
+
+	/**
+	 * @covers credly_get_api_key()
+	 */
+	public function credly_get_api_key() {}
+
+	/**
+	 * @covers credly_is_achievement_giveable()
+	 */
+	public function credly_is_achievement_giveable() {}
+
+	/**
+	 * @covers credly_fieldmap_get_fields()
+	 */
+	public function credly_fieldmap_get_fields() {}
+
+	/**
+	 * @covers credly_fieldmap_list_options()
+	 */
+	public function credly_fieldmap_list_options() {}
+
+	/**
+	 * @covers credly_fieldmap_get_field_value()
+	 */
+	public function credly_fieldmap_get_field_value() {}
+
+}
---
+++ tests/phpunit/tests/BadgeOS_Plugin_Updater_Test.php
@@ -0,0 +1,35 @@
+<?php
+
+class BadgeOS_Plugin_Updater_Test extends WP_UnitTestCase {
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::validate_license()
+	 */
+	public function test_BadgeOS_Plugin_Updater_validate_license() {}
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::register_licensed_addon()
+	 */
+	public function test_BadgeOS_Plugin_Updater_register_licensed_addon() {}
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::api_request()
+	 */
+	public function test_BadgeOS_Plugin_Updater_api_request() {}
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::activate_license()
+	 */
+	public function test_BadgeOS_Plugin_Updater_activate_license() {}
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::deactivate_license()
+	 */
+	public function test_BadgeOS_Plugin_Updater_deactivate_license() {}
+
+	/**
+	 * @covers BadgeOS_Plugin_Updater::check_license()
+	 */
+	public function test_BadgeOS_Plugin_Updater_check_license() {}
+
+}
