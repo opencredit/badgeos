7277c7092f66c659c712cd6ec768477ff2d2332b
Michael Beckwith <tw2113@gmail.com>
2014-04-28 18:20:55 -0500

    add some parts of the eventual tests

--- tests/phpunit/tests/BadgeOS_User_Functions_Test.php
+++ tests/phpunit/tests/BadgeOS_User_Functions_Test.php
@@ -5,12 +5,24 @@ class BadgeOS_User_Functions_Test extends WP_UnitTestCase {
 	/**
 	 * @covers badgeos_get_user_achievements()
 	 */
-	public function test_badgeos_get_user_achievements() {}
+	public function test_badgeos_get_user_achievements() {
+		//Need to set up some data.
+
+		$user_id = $this->factory->user->create();
+		$updated = badgeos_update_user_achievements( array( $user_id, ));
+		$this->assertNotEmpty( $updated );
+	}
 
 	/**
 	 * @covers badgeos_update_user_achievements()
 	 */
-	public function test_badgeos_update_user_achievements() {}
+	public function test_badgeos_update_user_achievements() {
+		//Need to set up an achievement here
+
+		$user_id = $this->factory->user->create();
+		$updated = badgeos_update_user_achievements( array( $user_id, ));
+		$this->assertNotEmpty( $updated );
+	}
 
 	/**
 	 * @covers badgeos_process_user_data()
