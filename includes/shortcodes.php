77f1c01e9120d8f7e11c923587e6245498458daa
Scott Kingsley Clark <lol@scottkclark.com>
2014-05-23 20:39:05 -0500

    Abstract the world. Also, create badgeos_shortcode_submissions_handler to handle the submissions/nominations shortcodes, because they're basically doing the same thing with a four line difference

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -134,3 +134,114 @@ function badgeos_shortcode_help_render_example( $shortcode = array() ) {
 function badgeos_shortcode_help_attributes( $key, $value ) {
 	return "{$key}=\"$value\"";
 }
+
+
+/**
+ * Handle Submissions/Nominations shortcode output
+ *
+ * @since  1.4.0
+ *
+ * @param array $atts Array of shortcode attributes.
+ * @param string $type Submission type.
+ * @param string $shortcode Shortcode name.
+ * @param array $defaults_override Array of overrides to set for default attributes.
+ *
+ * @return string
+ */
+function badgeos_shortcode_submissions_handler( $atts = array(), $type = 'submission', $shortcode = '', $defaults_override = array() ) {
+
+	// Setup defaults and allow override
+	$defaults = array_merge(
+		array(
+			'type'             => $type,
+			'limit'            => '10',
+			'status'           => 'all',
+			'show_filter'      => true,
+			'show_search'      => true,
+			'show_attachments' => true,
+			'show_comments'    => true,
+			'wpms'             => false
+		),
+		$defaults_override
+	);
+
+	// Parse shortcode attributes
+	$atts = shortcode_atts( $defaults, $atts, $shortcode );
+
+	// Get initial feedback HTML
+	$feedback_html = badgeos_render_feedback( $atts );
+
+	// Setup $badgeos_feedback array
+	$badgeos_feedback = $atts;
+
+	// Set current user ID
+	$badgeos_feedback[ 'user_id' ] = get_current_user_id();
+
+	// AJAX url
+	$badgeos_feedback[ 'ajax_url' ] = esc_url( admin_url( 'admin-ajax.php', 'relative' ) );
+
+	// AJAX data
+	$badgeos_feedback[ 'ajax_data' ] = array(
+		'action'           => 'get-feedback',
+		'type'             => $badgeos_feedback[ 'type' ],
+		'limit'            => $badgeos_feedback[ 'limit' ],
+		'show_attachments' => $badgeos_feedback[ 'show_attachments' ],
+		'show_comments'    => $badgeos_feedback[ 'show_comments' ],
+		'wpms'             => $badgeos_feedback[ 'wpms' ],
+		'user_id'          => $badgeos_feedback[ 'user_id' ]
+	);
+
+	// Filter fields to get values from form
+	$badgeos_feedback[ 'filters' ] = array(
+		'status' => '',
+		'search' => ''
+	);
+
+	if ( $badgeos_feedback[ 'show_filter' ] ) {
+		$badgeos_feedback[ 'filters' ][ 'status' ] = '.badgeos-feedback-filter select';
+	}
+
+	if ( $badgeos_feedback[ 'show_search' ] ) {
+		$badgeos_feedback[ 'filters' ][ 'search' ] = '.badgeos-feedback-search-input';
+	}
+
+	/**
+	 * Filter $badgeos_feedback to allow for overrides processed by JavaScript.
+	 *
+	 * @since 1.4.0
+	 *
+	 * @param array $badgeos_feedback {
+	 *     Array of feedback options sent to JavaScript.
+	 *
+	 *     @type string $action AJAX action to use.
+	 *     @type string $type Type of submission to use (submission/notification/etc).
+	 *     @type int $limit Number of items to show.
+	 *     @type boolean $show_attachments Show attachments in the list.
+	 *     @type boolean $show_comments Show comments in the list.
+	 *     @type boolean $wpms Show feedback from other sites in the list.
+	 *     @type int $user_id User ID to base the list off of.
+	 *     @type array $filters List of filter names and selectors to send in request.
+	 * }
+	 * @param array $atts {
+	 *     Array of shortcode attributes that were used.
+	 *
+	 *     @type string $type Type of submission to use (submission/notification/etc).
+	 *     @type int $limit Number of items to show.
+	 *     @type boolean $show_filter Show filter in the list.
+	 *     @type boolean $show_search Show search in the list.
+	 *     @type boolean $show_attachments Show attachments in the list.
+	 *     @type boolean $show_comments Show comments in the list.
+	 *     @type boolean $wpms Show feedback from other sites in the list.
+	 * }
+	 * @param string $shortcode Which shortcode called the Feedback AJAX functionality.
+	 */
+	$badgeos_feedback = apply_filters( 'badgeos_feedback_ajax_js', $badgeos_feedback, $atts, $shortcode );
+
+	// Enqueue and localize our JS
+	wp_enqueue_script( 'badgeos-achievements' );
+	wp_localize_script( 'badgeos-achievements', 'badgeos_feedback', $badgeos_feedback );
+
+	// Return initial feedback HTML
+	return $feedback_html;
+
+}
