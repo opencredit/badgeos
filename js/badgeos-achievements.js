3279d4b6596d2efc63f12a1d83582549c28f2727
Michael Beckwith <tw2113@gmail.com>
2016-05-02 15:01:53 -0500

    isolate for frontend.

--- js/badgeos-achievements.js
+++ js/badgeos-achievements.js
@@ -83,7 +83,7 @@ jQuery( function( $ ) {
 				$( response.data.message ).appendTo( $button.parent() ).fadeOut( 3000 );
 				$( '.badgeos-feedback-' + $button.data( 'feedback-id' ) + ' .badgeos-feedback-status' ).html( response.data.status );
 				$( '.cmb_id__badgeos_submission_current td, .cmb_id__badgeos_nomination_current td' ).html( response.data.status );
-				$( '.badgeos-comment-date-by' ).html( '<span class="badgeos-status-label">Status:</span> '+ response.data.status );
+				$('.badgeos-feedback-' + $button.data('feedback-id') + ' .badgeos-comment-date-by').html('<span class="badgeos-status-label">Status:</span> ' + response.data.status);
 			}
 		} );
 	} );
