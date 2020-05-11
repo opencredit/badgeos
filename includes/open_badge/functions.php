de1f186ab17ca6c2b9ab80168335cfbbd9afae2b
Farooq Abdullah <farooq@wooninjas.com>
2020-05-12 00:52:21 +0500

    random code fixes

--- includes/open_badge/functions.php
+++ includes/open_badge/functions.php
@@ -180,7 +180,7 @@ function badgeos_ob_profile_add_column( $achievement ) {
 	echo '</td>';
 }
 add_action( 'badgeos_profile_achivement_add_column_data', 'badgeos_ob_profile_add_column', 10, 1 );
-
+ 
 function badgeos_ob_profile_add_column_heading( ) {
 	wp_enqueue_script('thickbox');
 	wp_enqueue_style('thickbox');
@@ -305,4 +305,209 @@ function badgeos_ob_get_achievement_entry($entry_id) {
 	$table_name = $wpdb->prefix . 'badgeos_achievements';
 	$result = $wpdb->get_row($wpdb->prepare("SELECT * FROM {$table_name} A WHERE 1=1 AND A.entry_id=%d", $entry_id), ARRAY_A);
 	return $result;
+}
+
+function badgeos_convertable_credly_achievements_list_count() {
+	
+	global $wpdb;
+	
+	$result = $wpdb->get_results("SELECT p.ID FROM `".$wpdb->prefix."posts` as p inner JOIN ".$wpdb->prefix."postmeta as pm on( p.ID=pm.post_id AND pm.meta_key='_badgeos_send_to_credly' ) where pm.meta_value='true' ", ARRAY_A);
+	$recs = array();
+	if( count( $result ) > 0 ) {
+		foreach( $result as $res ) {
+
+			$enable_baking = get_post_meta( $res['ID'], '_open_badge_enable_baking', true );
+			if( $enable_baking != 'true' )
+				$recs[] = $res['ID'];
+		}
+	}
+	
+	wp_send_json( array( 'total_recs'=>count($recs), 'recs' => $recs ) );
+}
+add_action( 'wp_ajax_badgeos_get_convertable_credly_achievements_list_count', 'badgeos_convertable_credly_achievements_list_count' );
+
+function badgeos_convert_credly_achievements_2_open_badge_callback() {
+	
+	global $wpdb;
+	$post_id 		= sanitize_text_field( $_REQUEST[ 'ID' ] );
+	$disable_credly = sanitize_text_field( $_REQUEST[ 'disable_credly' ] );
+	$achievement = get_post( $post_id );
+	if( $achievement ) {
+		
+		update_post_meta( $post_id, '_open_badge_enable_baking', 'true' );
+		update_post_meta( $post_id, '_open_badge_criteria', get_permalink( $post_id ) );
+
+		update_post_meta( $post_id, '_open_badge_include_evidence', ( get_post_meta( $post_id, '_badgeos_credly_include_evidence', true ) ? get_post_meta( $post_id, '_badgeos_credly_include_evidence', true ): 'false' ) );
+        update_post_meta( $post_id, '_open_badge_expiration', ( get_post_meta( $post_id, '_badgeos_credly_expiration', true ) ? get_post_meta( $post_id, '_badgeos_credly_expiration', true ) : '0' ) );
+		update_post_meta( $post_id, '_open_badge_expiration_type', 'Day' );
+		
+		update_post_meta( $post_id, '_open_badge_updated_from_credly', 'true' );
+		update_post_meta( $post_id, '_open_badge_updated_from_credly_at', current_time( 'mysql' ) );
+
+		if( $disable_credly == 'true' ) {
+			update_post_meta( $post_id, '_badgeos_send_to_credly', 'false' );
+		}
+
+		wp_send_json( array( "type" => "success" ) );
+	} else {
+		wp_send_json( array( "type" => "error", 'message'=> __( 'No such an achievement found.', 'badgeos' ) ) );
+	}
+}
+add_action( 'wp_ajax_badgeos_convert_credly_achievements_2_open_badge', 'badgeos_convert_credly_achievements_2_open_badge_callback' );
+
+function badgeos_open_badge_metabox_save( $post_id = 0 ) {
+	
+	// Verify nonce
+	if ( isset( $_POST['badgeos_ob_quick_bulk_edit_action'] ) && $_POST['badgeos_ob_quick_bulk_edit_action'] == 'yes' ) {
+
+		update_post_meta( $post_id, '_open_badge_enable_baking', $_POST['open_badge_enable_baking'] == '1'?'true':'false' );
+        update_post_meta( $post_id, '_open_badge_criteria', get_permalink( $post_id ) );
+        update_post_meta( $post_id, '_open_badge_include_evidence', $_POST['open_badge_include_evidence'] == '1'?'true':'false' );
+        update_post_meta( $post_id, '_open_badge_expiration', sanitize_text_field( $_POST['open_badge_expiration'] ) );
+        update_post_meta( $post_id, '_open_badge_expiration_type', sanitize_text_field( $_POST['open_badge_expiration_type'] ) );
+	}
+}
+add_action( 'save_post', 'badgeos_open_badge_metabox_save' );
+
+function badgeos_ob_bulk_action_handler(   ) {
+	
+	if( ! current_user_can('manage_options') ) {
+		return;
+	}
+
+	if( isset( $_REQUEST[ 'post' ] ) && is_array( $_REQUEST[ 'post' ] ) && count( $_REQUEST[ 'post' ] ) > 0 ) {
+		$post_ids = $_REQUEST[ 'post' ];
+		if( is_array( $post_ids ) &&  count( $post_ids ) > 0 ) {
+			foreach( $post_ids as $post_id ) {
+				
+				if( isset( $_REQUEST['open_badge_enable_baking']  ) ) {
+					update_post_meta( $post_id, '_open_badge_enable_baking', $_REQUEST['open_badge_enable_baking'] == '1'?'true':'false' );
+				}
+
+				update_post_meta( $post_id, '_open_badge_criteria', get_permalink( $post_id ) );
+				
+				if( isset( $_REQUEST['open_badge_include_evidence']  ) ) {
+					update_post_meta( $post_id, '_open_badge_include_evidence', $_REQUEST['open_badge_include_evidence'] == '1'?'true':'false' );
+				}
+
+				if( isset( $_REQUEST['open_badge_expiration']  ) && !empty( $_REQUEST['open_badge_expiration']  ) ) {
+					update_post_meta( $post_id, '_open_badge_expiration', sanitize_text_field( $_REQUEST['open_badge_expiration'] ) );
+				}
+
+				if( isset( $_REQUEST['open_badge_expiration_type']  ) && !empty( $_REQUEST['open_badge_expiration_type']  ) ) {
+					update_post_meta( $post_id, '_open_badge_expiration_type', sanitize_text_field( $_REQUEST['open_badge_expiration_type'] ) );
+				}
+			}
+		}
+		
+	}
+}
+
+if( isset( $_REQUEST['badgeos_ob_quick_bulk_edit_action'] ) && $_REQUEST[ 'badgeos_ob_quick_bulk_edit_action' ] == 'yes' ) {
+	add_action( 'admin_init', 'badgeos_ob_bulk_action_handler' );
+}
+
+// Add our text to the quick & bulk edit box
+add_action('quick_edit_custom_box', 'badgeos_ob_quick_edit_custom_box', 10, 2);
+add_action('bulk_edit_custom_box', 'badgeos_ob_quick_edit_custom_box', 10, 2);
+function badgeos_ob_quick_edit_custom_box($column_name, $post_type ) {
+	global $post;
+	$screen = get_current_screen();
+
+	if( '_open_badge_enable_baking' == $column_name ) {
+		
+		$achievement_types 				= badgeos_get_achievement_types_slugs();
+		$open_badge_enable_baking 		= get_post_meta( $post->ID, '_open_badge_enable_baking', true ); 
+		$open_badge_include_evidence    = ( get_post_meta( $post->ID, '_open_badge_include_evidence', true ) ? get_post_meta( $post->ID, '_open_badge_include_evidence', true ) : 'false' );
+		$open_badge_expiration          = ( get_post_meta( $post->ID, '_open_badge_expiration', true ) ? get_post_meta( $post->ID, '_open_badge_expiration', true ) : '0' );
+		$open_badge_expiration_type     = ( get_post_meta( $post->ID, '_open_badge_expiration_type', true ) ? get_post_meta( $post->ID, '_open_badge_expiration_type', true ) : '0' );
+	
+		if ( in_array( $post_type, $achievement_types ) ) {
+			?>	
+				<input type="hidden" name="badgeos_ob_quick_bulk_edit_action" value="yes" />
+				<fieldset class="inline-edit-col-right" style="margin-top: 0;">
+					<div class="inline-edit-col">
+						<div class="inline-edit-group">
+							<label class="alignleft">
+								<span class="title" for="open_badge_enable_baking"><?php _e( 'Badge Baking?', 'badgeos' ); ?></span>
+								<select id="open_badge_enable_baking" class="open_badge_enable_baking_blk_field" name="open_badge_enable_baking">
+									<option value="1" <?php selected( $open_badge_enable_baking, 'true' ); ?>><?php _e( 'Yes', 'badgeos' ) ?></option>
+									<option value="0" <?php selected( $open_badge_enable_baking, 'false' ); ?>><?php _e( 'No', 'badgeos' ) ?></option>
+								</select>
+							</label>
+						</div>
+					</div>
+					<div class="inline-edit-col sub_ob_badgeos_blk_fields">
+						<div class="inline-edit-group">
+							<label class="alignleft">
+								<span class="title" for="open_badge_include_evidence"><?php _e( 'Evidence', 'badgeos' ); ?></span>
+								<select id="open_badge_include_evidence" name="open_badge_include_evidence">
+									<option value="1" <?php selected( $open_badge_include_evidence, 'true' ); ?>><?php _e( 'Yes', 'badgeos' ) ?></option>
+									<option value="0" <?php selected( $open_badge_include_evidence, 'false' ); ?>><?php _e( 'No', 'badgeos' ) ?></option>
+								</select>
+							</label>
+						</div>
+		
+					</div>
+					<div class="inline-edit-col sub_ob_badgeos_blk_fields">
+						<div class="inline-edit-group">
+							<label class="alignleft">
+								<span class="title" for="open_badge_expiration"><?php _e( 'Expiration', 'badgeos' ); ?></span>
+								<input type="number" id="open_badge_expiration" size="3" min="0" max="365" name="open_badge_expiration" value="<?php echo $open_badge_expiration; ?>" class="date_picker_class" />
+								<select id="open_badge_expiration_type" name="open_badge_expiration_type">
+									<option value="Day" <?php selected( $open_badge_expiration_type, 'Day' ); ?>><?php _e( 'Day(s)', 'badgeos' ) ?></option>
+									<option value="Month" <?php selected( $open_badge_expiration_type, 'Month' ); ?>><?php _e( 'Month(s)', 'badgeos' ) ?></option>
+									<option value="Year" <?php selected( $open_badge_expiration_type, 'Year' ); ?>><?php _e( 'Year(s)', 'badgeos' ) ?></option>
+								</select>
+							</label>
+						</div>
+					</div>
+				</fieldset>
+			<?php
+		}
+	}
+}
+
+/*
+ * New columns
+ */
+//add_filter('manage_post_posts_columns', 'badgeos_ob_admin_post_columns');
+
+// the above hook will add columns only for default 'post' post type, for CPT:
+// manage_{POST TYPE NAME}_posts_columns
+function badgeos_ob_admin_post_columns( $column_array ) {
+ 
+	$column_array['_open_badge_enable_baking'] = 'Open Badge?';
+	$column_array['_badgeos_send_to_credly'] = 'Credly?';
+	return $column_array;
+}
+
+add_action('admin_init', 'badgeos_ob_admin_edit_achievements_filter' );
+function  badgeos_ob_admin_edit_achievements_filter(  ) {
+	$achievement_types 				= badgeos_get_achievement_types_slugs();
+	foreach( $achievement_types as $type ) {
+		add_filter('manage_edit-'.$type.'_columns', 'badgeos_ob_admin_post_columns');
+		add_action( 'manage_'.$type.'_posts_custom_column' , 'badgeos_ob_admin_post_columns_data', 10, 2 );
+	}
+}
+
+/*
+ * Populate our new columns with data
+ */
+add_action('manage_posts_custom_column', 'badgeos_ob_admin_post_columns_data', 10, 2);
+function badgeos_ob_admin_post_columns_data( $column_name, $id ) {
+	// if you have to populate more that one columns, use switch()
+	switch( $column_name ) :
+		case '_open_badge_enable_baking': {
+			$open_badge_enable_baking = get_post_meta( $id, '_open_badge_enable_baking', true );
+			echo $open_badge_enable_baking=='true'?__( 'Yes', 'badgeos' ):__( 'No', 'badgeos' );
+			break;
+		}
+		case '_badgeos_send_to_credly': {
+			$send_to_credly = get_post_meta( $id, '_badgeos_send_to_credly', true );
+			echo $send_to_credly=='true'?__( 'Yes', 'badgeos' ):__( 'No', 'badgeos' );
+			break;
+		}
+	endswitch;
+ 
 }
