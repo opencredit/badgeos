b53d1cae7f7b0e3e4047ab094def9b0d3f27d11b
qalbeeabbas <qalbeeabbas.webxity@gmail.com>
2020-10-29 22:52:11 +0500

    Added the rank widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -7,6 +7,7 @@ add_action( 'widgets_init', 'badgeos_register_widgets' );
 function badgeos_register_widgets() {
 
 	register_widget( 'earned_user_achievements_widget' );
+	register_widget( 'earned_user_ranks_widget' );
 	
 	if( badgeos_first_time_installed() ) {
 		register_widget( 'credly_credit_issuer_widget' );
@@ -14,6 +15,8 @@ function badgeos_register_widgets() {
 }
 
 require_once( badgeos_get_directory_path() .'includes/widgets/earned-user-achievements-widget.php' );
+require_once( badgeos_get_directory_path() .'includes/widgets/earned-user-ranks-widget.php' );
+
 if( badgeos_first_time_installed() ) {
 	require_once( badgeos_get_directory_path() .'includes/widgets/credly-credit-issuer-widget.php' );
 }
