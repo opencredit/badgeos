8af6678f182a7ecf39798a254a921bd73ba353a8
mohammad karrar <mohammad.karrar1995@hotmail.com>
2021-06-14 16:46:23 +0500

    Update trigger description label

--- includes/ranks/rank-steps-ui.php
+++ includes/ranks/rank-steps-ui.php
@@ -233,7 +233,7 @@ function badgeos_rank_req_steps_ui_html($step_id = 0, $post_id = 0 ) {
 
 		<?php do_action( 'badgeos_rank_req_steps_ui_html_after_count_text', $step_id, $post_id ); ?>
 
-		<div class="step-title"><label for="step-<?php echo $step_id; ?>-title"><?php _e( 'Label', 'badgeos' ); ?>:</label> <input type="text" name="step-title" id="step-<?php echo $step_id; ?>-title" class="title" value="<?php echo get_the_title( $step_id ); ?>" /></div>
+		<div class="step-title"><label for="step-<?php echo $step_id; ?>-title"><?php _e( 'Label', 'badgeos' ); ?>:</label> <input type="text" name="step-title" id="step-<?php echo $step_id; ?>-title" class="title" value="" /></div>
 		<span class="spinner spinner-step-<?php echo $step_id;?>"></span>
 	</li>
 	<?php
